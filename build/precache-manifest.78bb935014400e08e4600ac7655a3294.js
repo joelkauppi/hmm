6bfcb6b0db74cfefced245f2e9cbef3f1c3083c1
Joel Kauppi <joel.kauppi@live.fi>
2020-04-06 23:19:22 +0300

    correct build folder

---
+++ build/precache-manifest.78bb935014400e08e4600ac7655a3294.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a0804e7db3ac7560cb487810992282a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f87e9ce6fd81339cb447",
+    "url": "/static/css/main.c7054938.chunk.css"
+  },
+  {
+    "revision": "73649e5125482e8527c9",
+    "url": "/static/js/2.02a8565a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.02a8565a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f87e9ce6fd81339cb447",
+    "url": "/static/js/main.f0204f46.chunk.js"
+  },
+  {
+    "revision": "f326c947c2bb131ebc6d",
+    "url": "/static/js/runtime-main.7900c9ab.js"
+  }
+]);
